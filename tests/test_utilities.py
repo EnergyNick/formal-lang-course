3f98735e47ef4262e18dde40cd6888aa4ed21df9
EnergyNick <nsikachin@gmail.com>
2023-03-31 00:14:10 +0300

    Написал тесты для ОНФХ

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,7 +1,10 @@
 import filecmp
 import os.path
+import tempfile
+
 import cfpq_data as cfpq
 import pytest
+from pyformlang.cfg import CFG, Variable, Production, Terminal
 
 from project import utilities
 
@@ -50,3 +53,43 @@ def test_build_graph_to_file(tmp_path: str):
     )
 
     assert filecmp.cmp(actual_path, expected_path)
+
+
+def assert_cfgs(first: CFG, second: CFG):
+    assert first.start_symbol == second.start_symbol
+    assert first.productions == second.productions
+
+
+def test_cfg_from_file():
+    temp = tempfile.TemporaryFile()
+    temp.write(b"S -> A\nA -> B\nB -> b")
+    actual = utilities.parse_cfg_from_file(temp.name)
+    varS = Variable("S")
+    varA = Variable("A")
+    varB = Variable("B")
+    productions = {
+        Production(varA, [varB]),
+        Production(varS, [varA]),
+        Production(varB, [Terminal("b")]),
+    }
+    expected = CFG(productions=productions, start_symbol=varS)
+
+    assert_cfgs(actual, expected)
+
+
+def test_cfg_to_wcnf():
+    initial = CFG.from_text("S -> a b")
+    actual = utilities.convert_to_weak_form(initial)
+    excepted = CFG.from_text(
+        'S -> "VAR:a#CNF#" "VAR:b#CNF#" \n "VAR:a#CNF#" -> a \n "VAR:b#CNF#" -> b'
+    )
+
+    assert_cfgs(actual, excepted)
+
+
+def test_cfg_to_wcnf_complex():
+    initial = CFG.from_text("S -> A S B | c\nB -> b\nA -> a")
+    actual = utilities.convert_to_weak_form(initial)
+    excepted = CFG.from_text('S -> A "VAR:C#CNF#1" | c\nC#CNF#1 -> S B\nB -> b\nA -> a')
+
+    assert_cfgs(actual, excepted)
