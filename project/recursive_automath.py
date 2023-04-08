bad9c1441c4eb750bee80e7c6010a75a6bb3f5d3
EnergyNick <nsikachin@gmail.com>
2023-04-08 15:40:22 +0300

    Сделал метод для конвертации в матрицу смежностей

--- project/recursive_automath.py
+++ project/recursive_automath.py
@@ -1,5 +1,6 @@
 from pyformlang.cfg import Variable
 from pyformlang.finite_automaton import EpsilonNFA
+from scipy.sparse import dok_matrix
 
 from project.ecfg import ECFG
 
@@ -33,3 +34,26 @@ class RecursiveAutomaton:
             head: body.to_epsilon_nfa() for head, body in ecfg.productions.items()
         }
         return cls(ecfg.start_symbol, elements)
+
+    def to_matrices(self):
+        """
+        Convert automath to adjacency matrix
+        :return: dictionary of states indexes and dictionary of adjacency matrix by symbol
+        """
+        states = dict()
+        matrix = dict()
+        count = sum(len(item.states) for item in self.boxes.values())
+        index = 0
+        for variable, automath in self.boxes.items():
+            for state in automath.states:
+                states[(variable, state)] = index
+                index += 1
+            for start, symbol, end in automath:
+                if symbol not in matrix:
+                    matrix[symbol] = dok_matrix((count, count), dtype=bool)
+                matrix[symbol][
+                    states[(variable, start)], states[(variable, end)]
+                ] = True
+
+        return states, matrix
+
--- tests/test_recursive_automath.py
+++ tests/test_recursive_automath.py
@@ -1,4 +1,5 @@
 from pyformlang.cfg import Variable, CFG
+from pyformlang.finite_automaton import State
 from pyformlang.regular_expression import Regex
 
 from project.ecfg import ECFG
@@ -10,10 +11,10 @@ def test_convert_cfg_to_ecfg():
     initial = CFG.from_text("S -> A S B | c\nB -> b\nA -> a")
     ecfg = ECFG.from_cfg(initial)
     expected_production = {
-                Variable("S"): Regex("A S B | c"),
-                Variable("B"): Regex("b"),
-                Variable("A"): Regex("a"),
-            }
+        Variable("S"): Regex("A S B | c"),
+        Variable("B"): Regex("b"),
+        Variable("A"): Regex("a"),
+    }
     for key in expected_production.keys():
         actual = automath.build_minimal_dfa_from_regex(ecfg.productions[key])
         expected = automath.build_minimal_dfa_from_regex(expected_production[key])
@@ -39,3 +40,14 @@ def test_convert_ecfg_to_recursive_automath():
         if actual.boxes[key].is_empty() and expected.is_empty():
             continue
         assert actual.boxes[key].is_equivalent_to(expected)
+
+
+def test_convert_to_adjacency_matrices():
+    recursive = RecursiveAutomaton.from_ecfg(ECFG.from_text("S -> a S b | c"))
+    states, matrix = recursive.to_matrices()
+    assert len(states) == 10
+    assert len(matrix) == 5
+    assert len(matrix[State("S")]) == 1
+    assert len(matrix[State("a")]) == 1
+    assert len(matrix[State("b")]) == 1
+    assert len(matrix[State("c")]) == 1
