b241ca0c3f768c1bf58a29ef3c08acd3930ab42b
EnergyNick <nsikachin@gmail.com>
2023-04-25 02:40:18 +0300

    Сделал реализацию основного матричного метода

--- project/grammatics.py
+++ project/grammatics.py
@@ -1,8 +1,10 @@
-from typing import Union
+from typing import Union, Callable, Any
 
+import numpy as np
 import pydot
 from networkx import MultiDiGraph, drawing
-from pyformlang.cfg import CFG, Variable
+from pyformlang.cfg import CFG, Variable, Epsilon, Terminal
+from scipy.sparse import dok_array, csr_array
 
 
 def convert_to_weak_form(cfg: CFG) -> CFG:
@@ -11,33 +13,6 @@ def convert_to_weak_form(cfg: CFG) -> CFG:
     weak_cfg = cleared_cfg._get_productions_with_only_single_terminals()
     weak_cfg = cleared_cfg._decompose_productions(weak_cfg)
     return CFG(start_symbol=cleared_cfg.start_symbol, productions=set(weak_cfg))
-
-
-def query_graph_with_cfg(
-    graph: MultiDiGraph,
-    cfg: Union[CFG, str],
-    start_nodes: set = None,
-    final_nodes: set = None,
-    start_symbol: Variable = Variable("S"),
-):
-    """
-    Query finite automaton with a context-free grammar
-
-    :param graph: Graph to query. If value will be string, method convert it to MultiDiGraph
-    :param cfg: Context-free grammar. If value will be string, method convert it to CFG
-    :param start_nodes: Start nodes in graph
-    :param final_nodes: End nodes in graph
-    :param start_symbol: Start symbol in context-free grammar
-    :return: Set of start and end pairs, where can be accessible by query
-    """
-    start_nodes = graph.nodes if start_nodes is None else start_nodes
-    final_nodes = graph.nodes if final_nodes is None else final_nodes
-
-    return {
-        (u, v)
-        for (variable, u, v) in hellings_algorithm(graph, cfg)
-        if variable == start_symbol and u in start_nodes and v in final_nodes
-    }
 
 
 def hellings_algorithm(graph: Union[MultiDiGraph, str], cfg: Union[CFG, str]):
@@ -96,3 +71,92 @@ def hellings_algorithm(graph: Union[MultiDiGraph, str], cfg: Union[CFG, str]):
                         queue.append(closure)
         result = result.union(to_append)
     return result
+
+
+def matrix_algorithm(graph: Union[MultiDiGraph, str], cfg: Union[CFG, str]):
+    """
+    Apply matrix algorithm to a graph
+
+    :param graph: Graph to query. If value will be string, method convert it to MultiDiGraph
+    :param cfg: Context-free grammar. If value will be string, method convert it to CFG
+    :return: set of tuples with variable, starting and ending node
+    """
+    if isinstance(graph, str):
+        graph = drawing.nx_pydot.from_pydot(pydot.graph_from_dot_data(graph)[0])
+    if isinstance(cfg, str):
+        cfg = CFG.from_text(cfg)
+
+    cfg = convert_to_weak_form(cfg)
+
+    epsilons_prod = set()
+    term_prod: dict = {}
+    var_prod = set()
+    for p in cfg.productions:
+        if len(p.body) == 1:
+            term_prod.setdefault(p.body[0].value, set()).add(p.head)
+        elif len(p.body) == 2:
+            var_prod.add((p.head, p.body[0].value, p.body[1].value))
+        else:
+            epsilons_prod.add(p.head)
+
+    points = {n: i for i, n in enumerate(graph.nodes)}
+    size = len(points)
+    matrixByVar = {var: dok_array((size, size), dtype=bool) for var in cfg.variables}
+
+    for var1, var2, label in graph.edges.data("label"):
+        p1 = points[var1]
+        p2 = points[var2]
+        for var in term_prod.setdefault(label, set()):
+            matrixByVar[var][p1, p2] = True
+
+    for matrix in matrixByVar.values():
+        matrix.tocsr()
+
+    diagonale = csr_array(np.eye(size, dtype=bool))
+    for var in epsilons_prod:
+        matrixByVar[var] += diagonale
+
+    isModified = True
+    while isModified:
+        isModified = False
+        for var, n1, n2 in var_prod:
+            prev = matrixByVar[var].nnz
+            matrixByVar[var] += matrixByVar[n1] @ matrixByVar[n2]
+            isModified |= matrixByVar[var].nnz != prev
+
+    result = set()
+    points = {i: n for n, i in points.items()}
+    for var, matrix in matrixByVar.items():
+        for p1, p2 in zip(*matrix.nonzero()):
+            result.add((var, points[p1], points[p2]))
+    return result
+
+
+def query_graph_with_cfg(
+    graph: MultiDiGraph,
+    cfg: Union[CFG, str],
+    start_nodes: set = None,
+    final_nodes: set = None,
+    start_symbol: Variable = Variable("S"),
+    algorithm: Callable[[MultiDiGraph, CFG], set[tuple[Variable, int, int]]] = hellings_algorithm
+):
+    """
+    Query finite automaton with a context-free grammar
+
+    :param graph: Graph to query. If value will be string, method convert it to MultiDiGraph
+    :param cfg: Context-free grammar. If value will be string, method convert it to CFG
+    :param start_nodes: Start nodes in graph
+    :param final_nodes: End nodes in graph
+    :param start_symbol: Start symbol in context-free grammar
+    :param algorithm:
+    :return: Set of start and end pairs, where can be accessible by query
+    """
+    start_nodes = graph.nodes if start_nodes is None else start_nodes
+    final_nodes = graph.nodes if final_nodes is None else final_nodes
+
+    return {
+        (u, v)
+        for (variable, u, v) in algorithm(graph, cfg)
+        if variable == start_symbol and u in start_nodes and v in final_nodes
+    }
+
